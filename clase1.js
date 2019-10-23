09225b4a8f769cabd4b64e29046848a1ae6fb766
Luis david <david12.29@hotmail.com>
2019-10-22 21:04:21 -0500

    i made a changed

--- clase1.js
+++ clase1.js
@@ -1,3 +1,6 @@
-var nombre="sacha", apellido="lifszyc"
+var nombre="sacha", apellido="Lifszyc"
 var nombreeEnMayusculas=nombre.toUpperCase()
 var apellidoEnMinusculas=apellido.toLowerCase()
+var primerLetraDelNombre=nombre.charAt(0)
+var cantidadDeLetrasDelNombre=nombre.length
+var nombreCompleto=nombre+apellido
