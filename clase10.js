f972f693682faccd3ffd150402fc89ea29f4b5ac
Luis david <david12.29@hotmail.com>
2019-10-26 18:09:11 -0500

    changed

--- clase10.js
+++ clase10.js
@@ -10,5 +10,25 @@ var sacha={
     drone:true
 }
 function imprimirProfesiones(personas) {
-
+    console.log(` ${persona.nombre} es:`)
+    if (persona.ingeniero) {
+        console.log("Ingeniero")
+    }
+    if (persona.cocinero) {
+        console.log("Cocinero")
+    }
+    if (persona.cantante) {
+        console.log("Cantante")
+    }
+    if (persona.dj) {
+        console.log("Dj")
+    }
+    if (persona.guitarrista) {
+        console.log("Guitarrista")
+    }
+    if (persona.drone) {
+        console.log("Drone")
+    }
 }
+
+ 
