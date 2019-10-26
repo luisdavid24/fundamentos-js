73ee07150fe1eb352f9da49568a376d8d9906626
Luis david <david12.29@hotmail.com>
2019-10-26 17:42:16 -0500

    profection

---
+++ clase10.js
@@ -0,0 +1,14 @@
+var sacha={
+    nombre:"sacha",
+    apellido:"Lopez",
+    edad:19,
+    ingeniero:true,
+    cocinero:false,
+    cantante:false,
+    dj:false,
+    guitarrista:false,
+    drone:true
+}
+function imprimirProfesiones(personas) {
+
+}
