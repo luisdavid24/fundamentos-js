f71660a1b886f77d08f039e6459f2623a1fb6b16
Luis david <david12.29@hotmail.com>
2019-10-28 18:14:11 -0500

    fin arrays

--- clase19.js
+++ clase19.js
@@ -1,27 +1,32 @@
 var david={
     nombre:"david",
     apellido:"lopez",
-    altura:1.70
+    altura:1.70,
+    libros:144
 }
 var andres={
     nombre:"andres",
     apellido:"gomez",
-    altura:1.85
+    altura:1.85,
+    libros:17
 }
 var laura={
     nombre:"laura",
     apellido:"castro",
-    altura:1.60
+    altura:1.60,
+    libros:4
 }
 var eli={
     nombre:"eli",
     apellido:"mendoza",
-    altura:1.65
+    altura:1.65,
+    libros:5
 }
 var edgar={
     nombre:"edgar",
     apellido:"montana",
-    altura:1.90
+    altura:1.90,
+    libros:9
 }
 // const esAlta=(persona)=>persona.altura>1.8
 const esAlta=({altura})=>altura>1.8
@@ -51,3 +56,7 @@ const pasarAlturaACms=persona=>{
 
 var personasCms=personas.map(pasarAlturaACms)
 console.log(personasCms)
+
+const reducer=(acum,{libros})=>acum+libros
+var totalDeLibros=personas.reduce(reducer,0)
+console.log( `Hay un total de ${totalDeLibros} libros entre todas las personas `)
