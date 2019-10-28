d6eb95fdbb4473a4583762f085955b93f8892229
Luis david <david12.29@hotmail.com>
2019-10-28 17:44:00 -0500

    changed again

---
+++ clase18.js
@@ -0,0 +1,47 @@
+var david={
+    nombre:"david",
+    apellido:"lopez",
+    altura:1.70
+}
+var andres={
+    nombre:"andres",
+    apellido:"gomez",
+    altura:1.85
+}
+var laura={
+    nombre:"laura",
+    apellido:"castro",
+    altura:1.60
+}
+var eli={
+    nombre:"eli",
+    apellido:"mendoza",
+    altura:1.65
+}
+var edgar={
+    nombre:"edgar",
+    apellido:"montana",
+    altura:1.90
+}
+// const esAlta=(persona)=>persona.altura>1.8
+const esAlta=({altura})=>altura>1.8
+const esBaja=({altura})=>altura<1.61
+
+var personas=[edgar,eli,laura,andres,david]
+var personasAltas=personas.filter(esAlta)
+
+// console.log(personasAltas)
+
+var personasBajas=personas.filter(esBaja)
+// console.log(personasBajas)
+
+const pasarAlturaACms=persona=>{
+    // persona.altura=persona.altura*100
+    // persona.altura *=100
+    return{
+        ...persona,
+        altura:persona.altura*100
+    }
+}
+var personasCms=personas.map(pasarAlturaACms)
+console.log(personasCms)
