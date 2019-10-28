87c7737a0e25df9e2f9a647104ca13b6c3b45c14
Luis david <david12.29@hotmail.com>
2019-10-27 22:40:44 -0500

    changed

--- clase12.js
+++ clase12.js
@@ -7,10 +7,11 @@ var david={
 }
 console.log(`Al inicio del año ${david.nombre} pesa ${david.peso}KG`)
 var INCREMENTO_PESO=0.2
+var DIAS_DEL_ANO=365
 const aumentarDePeso= persona => persona.peso+=INCREMENTO_PESO
 const adelgazar= persona => persona.peso-=INCREMENTO_PESO
 
-for (var i=1; i<=365;i++) {
+for (var i=1; i<=DIAS_DEL_ANO;i++) {
     var random=Math.random()
     if (random<.25) {
         aumentarDePeso(david)
