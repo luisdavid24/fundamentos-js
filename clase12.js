6b223dcaaa653bb3adeebe99df1272b8672551a5
Luis david <david12.29@hotmail.com>
2019-10-27 21:54:14 -0500

    function with for

--- clase12.js
+++ clase12.js
@@ -6,3 +6,18 @@ var david={
 
 }
 console.log(`Al inicio del año ${david.nombre} pesa ${david.peso}KG`)
+nst INCREMENTO_PESO=200
+const aumentarDePeso= persona => persona.peso+=INCREMENTO_PESO
+const adelgazar= persona => persona.peso-=INCREMENTO_PESO
+
+for (var i=1; i<=365;i++) {
+    var random=math.random()
+    if (random<.25) {
+        aumentarDePeso(david)
+    }
+    else if(random<.5) {
+        adelgazar(david)
+    }
+}
+
+console.log(`Al final del año ${david.nombre} pesa ${david.peso}KG`)
