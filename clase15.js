1a54aef0ee0aa2aa644aedca74892e8b3c9fc3b5
Luis david <david12.29@hotmail.com>
2019-10-28 15:40:20 -0500

    Switch

---
+++ clase15.js
@@ -0,0 +1,13 @@
+var signo= prompt("¿Cual es tu signo?")
+
+switch(signo) {
+    case "Aries":
+        console.log("Será esencial para este día que empiece a controlar cada una de las emociones y así evitar que estas afecten de manera negativa su ánimo.")
+        break
+    case "Tauro":
+        console.log("Durante este día, lo más probable es que toda su fuerza esté dirigida en la búsqueda de la orientación para su vocación o estudios específicos.")
+        break
+    case "Geminis":
+        console.log("En esta jornada deberá encontrar el equilibrio en su vida. Antes que nada, sepa que deberá determinar cuáles son las prioridades y lo más importante para usted.")
+        break
+}
