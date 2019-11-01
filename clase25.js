b215588d0e58a3455153e3033d39eac6899fc390
Luis david <david12.29@hotmail.com>
2019-10-31 20:52:10 -0500

    changed

--- clase25.js
+++ clase25.js
@@ -2,4 +2,8 @@ const API_URL="https://swapi.co/api/"
 const PEOPLE_URL="people/:id"
 const lukeUrl=`${API_URL}${PEOPLE_URL.replace(":id",1)}`
 const opts={crossDomain:true}
-$.get(lukeUrl,opts )
+const onPeopleResponse=function(persona){
+    console.log(`Hola soy ${persona.name}`)
+}
+
+$.get(lukeUrl,opts, onPeopleResponse )
