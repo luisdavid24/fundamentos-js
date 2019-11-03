2588a0fac6117c4c0179610a3b060a79ae42905c
Luis david <david12.29@hotmail.com>
2019-11-02 20:54:57 -0500

    changed

--- clase29.js
+++ clase29.js
@@ -1,6 +1,5 @@
 const API_URL="https://swapi.co/api/"
 const PEOPLE_URL="people/:id"
-// const lukeUrl=`${API_URL}${PEOPLE_URL.replace(":id",1)}`
 const opts={crossDomain:true}
 
 function obtenerPersonaje(id) {
